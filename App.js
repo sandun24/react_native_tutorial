910bfd3f708343fd7274ca1c028ec319d6e1521f
sandun24 <vidurangaperera1@gmail.com>
2021-02-02 13:59:24 +0530

    touchable content

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useState} from 'react';
-import { Button, FlatList, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Button, FlatList, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
 
   const [people, setPeople] = useState([
     { name: 'shawn', key: '1'},
-    { name: 'paul', key: '2'},
+    { name: 'paul', key: '2'},   
     { name: 'joun', key: '3'},
     { name: 'peter', key: '4'},
     { name: 'mark', key: '5'},
@@ -14,13 +14,21 @@ export default function App() {
     { name: 'don', key: '7'},
   ]);
 
+  const pressHandler = (id) =>{
+    console.log(id);
+  }
+
   return (
     <View style={styles.container}>
       
       <FlatList
+        numColumns={2}
         data={people}
         renderItem={({item}) => (
-          <Text style={styles.item}>{item.name}</Text>
+          <TouchableOpacity onPress={() => pressHandler(item.key)}>
+            <Text style={styles.item}>{item.name}</Text>
+          </TouchableOpacity>
+          
         )}  
       />
       {/* <ScrollView>
@@ -58,6 +66,8 @@ const styles = StyleSheet.create({
     marginTop: 24,
     padding: 30,
     backgroundColor: 'pink',
-    fontSize: 24
+    fontSize: 24,
+    marginHorizontal:10,
+    marginTop: 24
   }
 });
